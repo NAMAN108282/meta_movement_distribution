d9dc61e183be1a433527b3304ae68bf3f4c93b71
Naman <49680172+NAMAN108282@users.noreply.github.com>
2024-04-15 12:54:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 1.0"
-  backend "local" {}  # Can change from "local" to "gcs" (for google) or "s3" (for aws), if you would like to preserve your tf-state online
   required_providers {
     google = {
       source  = "hashicorp/google"
@@ -11,16 +10,15 @@ terraform {
 provider "google" {
   project = var.project
   region = var.region
-  // credentials = file(var.credentials)  # Use this if you do not want to set env-var GOOGLE_APPLICATION_CREDENTIALS
+  // credentials = file(var.credentials)  # Alternatively set GOOGLE_APPLICATION_CREDENTIALS
 }
 
 # Data Lake Bucket
-# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
 resource "google_storage_bucket" "data-lake-bucket" {
-  name          = "${local.data_lake_bucket}_${var.project}" # Concatenating DL bucket & Project name for unique naming
+  name          = "${local.data_lake_bucket}_${var.project}"
   location      = var.region
 
-  # Optional, but recommended settings:
+  # Optional bucket settings:
   storage_class = var.storage_class
   uniform_bucket_level_access = true
 
@@ -40,7 +38,7 @@ resource "google_storage_bucket" "data-lake-bucket" {
   force_destroy = true
 }
 
-
+# DataProc Cluster settings
 resource "google_dataproc_cluster" "simplecluster" {
   name   = var.dataproc_cluster_name
   region = var.region
@@ -56,10 +54,14 @@ resource "google_dataproc_cluster" "simplecluster" {
     worker_config {
       num_instances = 0
       machine_type  = "n2-standard-2"
+      disk_config {
+        boot_disk_size_gb = 30
+      }
     }
   }
 }
 
+# Big Query Dataset 
 resource "google_bigquery_dataset" "dataset" {
   dataset_id = var.bq_dataset
   project    = var.project
