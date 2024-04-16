eab7c027165da9c70c04e341affd65f8e7524959
Naman <49680172+NAMAN108282@users.noreply.github.com>
2024-04-16 11:52:48 +0530

    Update config.py

--- code/config.py
+++ code/config.py
@@ -1,6 +1,6 @@
 import json
 
-def read_local_config(config_file_path: str= "code/config.json"):
-    with open(config_file_path, "r") as file:
+def read_config(path: str= "code/config.json"):
+    with open(path, "r") as file:
         config = json.load(file)
     return config
